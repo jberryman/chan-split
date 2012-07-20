00424a820db04e48e94f8ec9208c5cc14f268674
Brandon Simmons <brandon.m.simmons@gmail.com>
2012-07-20 09:58:48 -0400

    implement from scratch, after lpsmith's implementation

--- Control/Concurrent/Chan/Split.hs
+++ Control/Concurrent/Chan/Split.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses, DeriveDataTypeable #-}
 module Control.Concurrent.Chan.Split (
     -- * Chan pairs
       InChan()
@@ -11,65 +11,73 @@ module Control.Concurrent.Chan.Split (
 
     ) where
 
-import qualified Control.Concurrent.Chan as C
-import Data.Functor.Contravariant
-import Control.Applicative
-import Control.Arrow
+--
+-- inspired by Leon P. Smith's from-scratch implementation
+-- copy-pasta from Control.Concurrent.Chan
+--
+
+import System.IO.Unsafe ( unsafeInterleaveIO )
+import Control.Concurrent.MVar
+import Control.Exception (mask_)
+import Data.Typeable
+
 import Control.Concurrent.Chan.Class
 
+type Stream a = MVar (ChItem a)
+data ChItem a = ChItem a (Stream a)
 
+-- | The \"write side\" of a chan pair
+newtype InChan i = InChan (MVar (Stream i)) -- Invariant: Stream i always empty
+    deriving (Eq, Typeable)
 
--- TODO: test performance of this with and without fmaped / contramap values in
--- comparison with standard Chan. Test to see if we can improve performance
--- using special constructor for fmaped / contramap version
-
-
--- | The "write side" of a chan pair
-data InChan i where
-    InChan :: (i -> a) -> C.Chan a -> InChan i
-
--- | The "read side" of a chan pair
-data OutChan o where
-    OutChan :: (a -> o) -> C.Chan a -> OutChan o
+-- | The \"read side\" of a chan pair
+newtype OutChan i = OutChan (MVar (Stream i)) 
+    deriving (Eq, Typeable)
 
 instance NewSplitChan InChan OutChan where
     -- | Create corresponding read and write ends of a chan pair. Writes to the
     -- 'InChan' side can be read on the 'OutChan' side.
-    newSplitChan = (InChan id &&& OutChan id) <$> C.newChan
+    newSplitChan = do
+       hole  <- newEmptyMVar
+       readVar  <- newMVar hole
+       writeVar <- newMVar hole
+       return ( InChan writeVar, OutChan readVar )
 
 
 instance SplitChan InChan OutChan where
-    writeChan (InChan f c) = C.writeChan c . f
-    writeList2Chan (InChan f c) = C.writeList2Chan c . map f
-    readChan (OutChan f c) = f <$> C.readChan c 
+    writeChan (InChan writeVar) val = do
+          new_hole <- newEmptyMVar
+          mask_ $ do
+            old_hole <- takeMVar writeVar
+            putMVar old_hole (ChItem val new_hole)
+            putMVar writeVar new_hole
 
-instance Contravariant InChan where
-    contramap f' (InChan f c) = InChan (f . f') c
+    writeList2Chan ch ls = sequence_ (map (writeChan ch) ls)
 
-instance Functor OutChan where
-    fmap f' (OutChan f c) = OutChan (f' . f) c
-
-
+    readChan (OutChan readVar) = do
+          modifyMVar readVar $ \read_end -> do
+            (ChItem val new_read_end) <- readMVar read_end
+                -- Use readMVar here, not takeMVar,
+                -- else dupChan doesn't work
+            return (new_read_end, val)
 
 
 -- | Return a lazy list representing the contents of the supplied OutChan, much
 -- like System.IO.hGetContents.
 getChanContents :: OutChan a -> IO [a]
-getChanContents (OutChan f c) = map f <$> C.getChanContents c
+getChanContents ch = unsafeInterleaveIO (do
+                            x  <- readChan ch
+                            xs <- getChanContents ch
+                            return (x:xs)
+                        )
 
-
--- | Duplicate an 'OutChan': the duplicate channel begins empty, but data
--- written to the corresponding 'InChan' will appear in both, i.e. consuming a
--- value from the copy will have no affect on the values in the original
--- OutChan.
+-- | Duplicate an 'OutChan': the duplicate channel contains any unread messages
+-- in the original (n.b. this differs from the behavior of dupChan in Chan),
+-- and data written to the corresponding 'InChan' will appear in both, i.e.
+-- consuming a value from the copy will have no affect on the values in the
+-- original OutChan.
+--
+-- (Note that a duplicated channel is not equal to its original.
+-- So: @fmap (c /=) $ dupChan c@ returns @True@ for all @c@.)
 dupChan :: OutChan a -> IO (OutChan a)
-dupChan (OutChan f c) = OutChan f <$> C.dupChan c
-
-{-
--- | EXPERIMENTAL: combine multiple output chans, interleaving their values
-mergeOutChans :: [OutChan a] -> IO (OutChan a)
-mergeOutChans cs = 
-    as <- mapM C.getChanContents cs
-    ...
--}
-
+dupChan (OutChan writeVar) = OutChan `fmap` withMVar writeVar newMVar
